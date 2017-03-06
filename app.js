05e08cac2a570afb00405101b594bddebcfd64aa
Immo Struchholz <mrerody@googlemail.com>
2017-03-06 19:21:10 +0100

    Add mongodb, verify url, return errors via json in case of error

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongodb = require('mongodb');
 const crypto = require('crypto');
+const validUrl = require('valid-url');
 
 const port = process.env.PORT || 3000;
 const url = 'mongodb://localhost:27017/url_shortener';
@@ -11,24 +12,39 @@ const app = express();
 MongoClient.connect(url, (err, db) => {
 	if(err) throw err;
 
+	const collection = db.collection('urls');
+
 	app.get('/', (req, res) => {
 		res.send('Pass a valid url to /new/ to receive a shortened url.');
 	});
 
 	app.get('/:url', (req, res) => {
-		// check db for shortened url
-			// if url is found redirect to the original url
-			// if url is not found respond with json(err)
+		const shortened = req.params.url;
+		collection.findOne({shortened}, (err, item) => {
+			if(err) throw err;
+			if(item){
+				return res.redirect(item.originalUrl);
+			} else {
+				res.json({error: 'Incorrect shortened URL.'})
+			}
+		});
 	});
 
 	app.get('/new/*', (req, res) => {
 		const originalUrl = req.params[0];
-		const shortened = generateShortUrl();
-		const shortenedUrl = `${req.protocol}://${req.get('host')}/${shortened}`;
-		res.json({originalUrl, shortenedUrl})
+
+		if(validUrl.isWebUri(originalUrl)) {
+			const shortened = generateShortUrl();
+			const shortenedUrl = `${req.protocol}://${req.get('host')}/${shortened}`;
+			res.json({originalUrl, shortenedUrl});
+			collection.update({ originalUrl }, { originalUrl, shortenedUrl, shortened }, { upsert: true })
+		} else {
+			res.json({error: 'Invalid URL.'});
+		}
+
 	});
 
-	db.close();
+
 });
 
 
